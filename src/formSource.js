c05a41708890c528e131266ba7df8ee0c8b3565e
Abdul-Razak, J <zakitworld@gmail.com>
2022-12-26 20:28:38 -0800

    Add files via upload

---
+++ src/formSource.js
@@ -0,0 +1,77 @@
+export const userInputs = [
+    {
+      id: 1,
+      label: "Username",
+      type: "text",
+      placeholder: "abdul_raz",
+    },
+    {
+      id: 2,
+      label: "Name and surname",
+      type: "text",
+      placeholder: "Jibril Abdul-Razak",
+    },
+    {
+      id: 3,
+      label: "Email",
+      type: "mail",
+      placeholder: "zakitworld@gmail.com",
+    },
+    {
+      id: 4,
+      label: "Phone",
+      type: "text",
+      placeholder: "+233 2434 567 89",
+    },
+    {
+      id: 5,
+      label: "Password",
+      type: "password",
+    },
+    {
+      id: 6,
+      label: "Address",
+      type: "text",
+      placeholder: "Dopass St. 216 Kadjebi",
+    },
+    {
+      id: 7,
+      label: "Country",
+      type: "text",
+      placeholder: "Ghana",
+    },
+  ];
+  
+  export const productInputs = [
+    {
+      id: 1,
+      label: "Title",
+      type: "text",
+      placeholder: "Apple Macbook Pro",
+    },
+    {
+      id: 2,
+      label: "Description",
+      type: "text",
+      placeholder: "Description",
+    },
+    {
+      id: 3,
+      label: "Category",
+      type: "text",
+      placeholder: "Computers",
+    },
+    {
+      id: 4,
+      label: "Price",
+      type: "text",
+      placeholder: "100",
+    },
+    {
+      id: 5,
+      label: "Stock",
+      type: "text",
+      placeholder: "in stock",
+    },
+  ];
+  
